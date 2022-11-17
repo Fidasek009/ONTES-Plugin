aecd1136746a2edb8bd1394077baa2c5c564dfec
Fidasek009 <102549834+Fidasek009@users.noreply.github.com>
2022-11-17 15:17:25 +0100

    Update ontes.js

--- ontes.js
+++ ontes.js
@@ -1,23 +1,40 @@
 // answers url
-const url = 'https://raw.githubusercontent.com/Fidasek009/ONTES-Plugin/main/ONTES-Answers.json';
+const SingeURL = 'https://raw.githubusercontent.com/Fidasek009/ONTES-Plugin/main/answers/.ONTES-Single-Answers.json';
+const MultiURL = 'https://raw.githubusercontent.com/Fidasek009/ONTES-Plugin/main/answers/.ONTES-Multi-Answers.json';
 
 function delay(time) {
   return new Promise(resolve => setTimeout(resolve, time));
 }
 
 async function loadAnswers() {
-	await delay(5000); // wait for site to load questions
+	await delay(2000); // wait for site to load questions
 	
 	// download answers
-    const response = await fetch(url);
-    const answers = await response.json();
+    const response = await fetch(SingeURL);
+    const SingleAnswers = await response.json();
 	
-	var questions = document.querySelectorAll('[typ="single"]'); // get all question DIVs
-	for (var i = questions.length - 1; i >= 0; i--)
+	var single = document.querySelectorAll('[typ="single"]'); // get all single questions
+	for (let i = single.length - 1; i >= 0; i--)
 	{
-		let otazka = questions[i].getAttribute("id"); // get id of question
-		document.getElementById(answers[otazka]).style.color = "red"; // get answer for question
+		let otazka = single[i].getAttribute("id"); // get id of question
+		document.getElementById(SingleAnswers[otazka]).style.color = "red"; // get answer for question
 	}
+	
+	var multi = document.querySelectorAll('[typ="multi"]'); // get all multi questions
+	if(multi.length > 0){
+		// download multi answers
+		const response = await fetch(MultiURL);
+		const MultiAnswers = await response.json();
+	
+		for (let i = multi.length - 1; i >= 0; i--)
+		{
+			let otazka = multi[i].getAttribute("id"); // get id of question
+			for(let j = 0; j < MultiAnswers[otazka].length; j++) {
+				document.getElementById(MultiAnswers[otazka][j]).style.color = "red"; // get answer for question
+			}
+		}
+	}
+	
 	console.log("done")
 }
 
